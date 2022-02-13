0730ae9e53b041b827004dbd7482d8528af3824b
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2022-02-13 18:32:36 +0900

    Update README.md file (#83)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ core.warning(
   'This GitHub action is deprecated, is not supported and cannot be used. ' +
   'Twilio sunset programmable fax offering on 2021/12/17. And also, ' +
   'twilio-node library introduced the breaking change in 3.74.0 version ' +
-  'where possibility to send faxes has been removed. More ' +
-  ' information: https://github.com/twilio/twilio-node/releases/tag/3.74.0' +
-  ' and https://www.twilio.com/changelog/programmable-fax-end-life-one-year-notice'
+  'where possibility to send faxes has been removed. More information: ' +
+  'https://github.com/twilio/twilio-node/releases/tag/3.74.0 and ' +
+  'https://www.twilio.com/changelog/programmable-fax-end-life-one-year-notice'
 )
