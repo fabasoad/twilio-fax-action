dd4bc33822af1bceb7d9096d7c48b48c1fa79534
fabasoad <fabasoad@gmail.com>
2022-02-13 18:19:43 +0900

    Deprecate twilio-fax-action 5

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ core.warning(
   'Twilio sunset programmable fax offering on 2021/12/17. And also, ' +
   'twilio-node library introduced the breaking change in 3.74.0 version ' +
   'where possibility to send faxes has been removed. More ' +
-  ' information here: [1] https://github.com/twilio/twilio-node/releases/tag/3.74.0' +
-  ' and [2] https://www.twilio.com/changelog/programmable-fax-end-life-one-year-notice'
+  ' information: https://github.com/twilio/twilio-node/releases/tag/3.74.0' +
+  ' and https://www.twilio.com/changelog/programmable-fax-end-life-one-year-notice'
 )
